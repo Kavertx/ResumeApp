92ea85d0cd19d7a4c14b32333a7096d85a61da83
Stoyan <terora66@gmail.com>
2023-04-14 16:07:43 +0300

    RecyclerView and Spinner are set up and properly displaying views Each item from the recyclerview is clickable and redirects to the correct github page

--- app/src/main/java/com/example/resume/ui/projects/ProjectsViewModel.kt
+++ app/src/main/java/com/example/resume/ui/projects/ProjectsViewModel.kt
@@ -6,8 +6,27 @@ import androidx.lifecycle.ViewModel
 
 class ProjectsViewModel : ViewModel() {
 
+    val resume = Project("Resume", "https://github.com/Kavertx/ResumeApp/")
+    val notes = Project("Notes", "https://github.com/Kavertx/NotesApp/")
+    val calculator = Project("Calculator", "https://github.com/Kavertx/Calculator/")
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    val techStackArray = arrayOf<String>("Android", "Web", "Desktop")
+
+    val mapOfProjects =  mapOf<String, Array<Project>>(
+        techStackArray[0] to arrayOf(resume, notes, calculator),
+        techStackArray[1] to arrayOf(),
+        techStackArray[2] to arrayOf()
+    )
+
+
+    val androidProjectsList = arrayOf<Project>(resume, notes, calculator)
+    val webProjectsList = arrayOf<Project>()
+    val desktopProjectsList = arrayOf<Project>()
 }
+
+
+class Project(val Name: String, val URL: String)

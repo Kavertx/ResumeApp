a26dd44fe003fb135764aac769912bc04b093034
Stoyan <terora66@gmail.com>
2023-09-20 16:24:03 +0300

    Added newly acquired certificates and corresponding images

--- app/src/main/java/com/example/resume/ui/certificates/CertificatesFragment.kt
+++ app/src/main/java/com/example/resume/ui/certificates/CertificatesFragment.kt
@@ -13,12 +13,36 @@ class CertificatesFragment : Fragment() {
 
     private var _binding: FragmentCertificatesBinding? = null
 
-    val androidKotlin2022 = Certificate("Android Development with Kotlin",
-        "https://softuni.bg/Certificates/Details/148201/bcf6ba8e")
-    val QAbasics = Certificate("QA Basics",
-        "https://softuni.bg/certificates/details/154752/77aba62a")
+    val androidKotlin2022 = Certificate(
+        "Android Development with Kotlin",
+        "https://softuni.bg/Certificates/Details/148201/bcf6ba8e"
+    )
+    val QaBasics = Certificate(
+        "QA Basics",
+        "https://softuni.bg/certificates/details/154752/77aba62a"
+    )
+    val softwareTechnologies = Certificate(
+        "Software Technologies",
+        "https://softuni.bg/certificates/details/175738/2be3fc79"
+    )
+    val QaFundamentalsAndManualTesting = Certificate(
+        "QA Fundamentals and Manual Testing",
+        "https://softuni.bg/certificates/details/183605/9b1d2d2d"
+    )
+    val htmlAndCss =
+        Certificate("HTML & CSS", "https://softuni.bg/certificates/details/182042/da0154ee")
+    val csAdvanced =
+        Certificate("C# Advanced", "https://softuni.bg/certificates/details/173566/d79312a4")
 
-    val certificatesArray = arrayOf(androidKotlin2022, QAbasics)
+
+    val certificatesArray = arrayOf(
+        androidKotlin2022,
+        QaBasics,
+        softwareTechnologies,
+        QaFundamentalsAndManualTesting,
+        htmlAndCss,
+        csAdvanced
+    )
 
     // This property is only valid between onCreateView and
     // onDestroyView.
